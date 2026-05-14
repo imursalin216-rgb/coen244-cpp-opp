018b180d660672f385ec2db58ebd92357a5692fc
imursalin216-rgb <imursalin216@gmail.com>
2026-05-14 11:32:54 -0400

    Add Date class with constructors and methods

--- assignments/assignment-1/assignments/assignment-1/Date.h
+++ assignments/assignment-1/assignments/assignment-1/Date.h
@@ -1,1 +1,32 @@
+#ifndef UNTITLED_DATE_H
+#define UNTITLED_DATE_H
+
+class Date {
+
+private:
+    int month;
+    int day;
+    int year;
+
+public:
+
+    Date();
+    Date(int m, int d, int y);
+    Date(const Date& other);
+    ~Date();
+
+    void setMonth(int m);
+    void setDay(int d);
+    void setYear(int y);
+
+
+    int getMonth() const;
+    int getDay() const;
+    int getYear() const;
+
+
+    void print() const;
+};
+
+#endif
 
